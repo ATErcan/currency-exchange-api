0819e2d186f4ac87f4c4216851d1d2bf7ddb05d8
talha <atercan0658@gmail.com>
2024-11-26 23:16:54 +0100

    [feature/auth-routes] - create auth routes and controller

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const authController = require("../controllers/authController");
+
+const router = Router();
+
+router.post("/auth/signup", authController.signup_post);
+router.post("/auth/login", authController.login_post);
+router.get("/auth/logout", authController.logout_get);
+
+module.exports = router;
