c958f08717472f42a413aa36a74a4b7ca079266c
talha <atercan0658@gmail.com>
2024-12-02 16:42:22 +0100

    [feature/financial-model] - create financial model, create userService

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,58 +1,11 @@
-const jwt = require("jsonwebtoken");
-
-const User = require("../models/User");
-const { JWT_SECRET, JWT_MAX_AGE } = require('../config/jwtConfig')
-
-const createToken = (data) => {
-  try {
-    return jwt.sign(data, JWT_SECRET, { expiresIn: JWT_MAX_AGE });
-  } catch (error) {
-    throw new Error("Failed to create token");
-  }
-}
-
-const handleErrors = (err) => {
-  const errors = { name: "", email: "", password: "" };
-  let status = 400;
-
-  // duplicate email error
-  if (err.code === 11000) {
-    status = 409;
-    errors.email = "Email already in use";
-    return {
-      status,
-      message: errors,
-    };
-  }
-
-  // validation errors
-  if (err.message.includes("user validation failed")) {
-    Object.values(err.errors).forEach(({ properties }) => {
-      errors[properties.path] = properties.message;
-    });
-  }
-
-  // Handle token creation errors
-  if (err.message === "Failed to create token") {
-    status = 500;
-    return {
-      status,
-      message:
-        "An error occurred while generating the token. Please try again.",
-    };
-  }
-
-  return {
-    status,
-    message: errors,
-  };
-}
+const createUser = require("../services/userService");
+const { createToken, handleErrors } = require("../utils/auth");
 
 const signup_post = async (req, res) => {
   const { name, email, password } = req.body;
   
   try {
-    const user = await User.create({ name, email, password });
+    const user = await createUser({ name, email, password });
     const token = createToken({
       id: user._id,
       name: user.name,
