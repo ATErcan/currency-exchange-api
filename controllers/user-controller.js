77976fbeadc2867ee3077eadba5634ce8041f2b5
talha <atercan0658@gmail.com>
2025-01-24 22:43:32 +0100

    [feature/update-user] - patch method created to update user info

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,6 +21,27 @@ const user_get = async (req, res) => {
   }
 }
 
+const user_patch = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const userId = req.userId;
+    const user = await User.findOne({ _id: userId });
+    if(!user) {
+      throw createError("User not found! If this error persist, please contact support", 404)
+    }
+    user.name = name;
+    await user.save();
+    const { password, ...userData } = user._doc;
+
+    res.status(200).json({
+      data: userData,
+    });
+  } catch(error) {
+    const { status, message } = handleErrors(error);
+    res.status(status).json({ message });
+  }
+}
+
 const all_transactions_get = async (req, res) => {
   try {
     const userId = req.userId;
@@ -55,4 +76,4 @@ const all_transactions_get = async (req, res) => {
   }
 }
 
-module.exports = { user_get, all_transactions_get };
+module.exports = { user_get, user_patch, all_transactions_get };
